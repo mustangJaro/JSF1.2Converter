1c42572e6f2613efa9413104122ea0160d71f6c9
mustangJaro <ajarosinski@gmail.com>
2016-03-25 15:57:54 -0500

    Copy file and perform SVN mv command
    This helps keep SVN history when performing a conversion

--- JSFConverter/src/com/convert/jsf/JSFConverter.java
+++ JSFConverter/src/com/convert/jsf/JSFConverter.java
@@ -2,6 +2,10 @@ package com.convert.jsf;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.apache.log4j.Logger;
 
@@ -20,11 +24,30 @@ public class JSFConverter {
 		//TODO loop through files
 		JspParser parser = new JspParser();
 		JSFParserHandler handler = new JSFParserHandler();
-		handler.setOutputDir(new File("c:\\xhtml"));
+		
+		String subDir = "\\";
+		String filename = "home.jsp";
+		handler.setIncludedFile(false);
+		
+
+		String outputDir = "c:\\xhtml\\";
+		handler.setOutputDir(new File(outputDir + subDir));
 		handler.setIncludePrimeFacesConversion(true);
-		File jsf12 = new File("C:\\workspace\\Version93 - JSF2\\CustomCallPortal\\www\\home.jsp");
+		File jsf12 = new File("C:\\workspace\\Version93 - JSF2\\CustomCallPortal\\www\\" + subDir + filename);
 		try {
 			parser.parse(jsf12, handler);
+			
+			//copy file to original location
+			Path targetFile = Paths.get(jsf12.getAbsolutePath());
+			Path sourceFile = Paths.get(outputDir + subDir + filename);
+			Files.copy(sourceFile, targetFile, StandardCopyOption.REPLACE_EXISTING);
+			
+			//perform SVN command to rename
+			Runtime rt = Runtime.getRuntime();
+			rt.exec("svn mv \"" + targetFile + "\" \"" + targetFile.toString().replace(".jsp", ".xhtml") + "\"");
+			
+			//TODO commit SVN file
+			//rt.exec("svn commit");
 		} catch (IOException e) {
 			logger.error("Failure parsing file: " + jsf12.getAbsolutePath(), e);
 		}
